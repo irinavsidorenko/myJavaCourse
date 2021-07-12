808252a27564691becfcdc63cc5403d668906bcd
Irina Sidorenko <koshkatonia@gmail.com>
2021-07-12 16:19:39 +0300

    several functions added

--- sandbox/src/main/java/ru/stqa/myJavaCourse/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/myJavaCourse/sandbox/MyFirstProgram.java
@@ -2,6 +2,25 @@ package ru.stqa.myJavaCourse.sandbox;
 
 public class MyFirstProgram {
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		hello("world");
+		hello("user");
+		hello("Irina");
+
+		double l = 5;
+		System.out.println("Площадь квадрата со стороной "+l+" = "+area(l));
+
+		double a = 4;
+		double b = 6;
+		System.out.println("Площадь прямоугольника со сторонами "+ a + " и " + b + " = " + area(a,b));
+		}
+
+	public static void hello(String somebody){
+		System.out.println("Hello, "+somebody+ "!");
+	}
+	public static double area(double length) {
+		return length*length;
+	}
+	public static double area(double a, double b){
+		return a*b;
 	}
 }
